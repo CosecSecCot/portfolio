7e3d866884acdf5a4fd9f97ed6e0463e5896832a
CosecSecCot <cosecseccot581@gmail.com>
2025-10-02 21:21:59 +0530

    fix type errors in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,7 @@ import Link, { LinkProps } from "next/link";
 import React, { ForwardedRef } from "react";
 
 type BaseButtonProps = {
-  children: React.ReactNode;
-  className?: string;
   buttonStyle?: "default" | "secondary";
-  disabled?: boolean;
 };
 
 type LinkButtonProps = BaseButtonProps &
@@ -20,40 +17,49 @@ type NativeButtonProps = BaseButtonProps &
 
 type ButtonProps = LinkButtonProps | NativeButtonProps;
 
+function isLinkProps(props: ButtonProps): props is LinkButtonProps {
+  return "href" in props;
+}
+
 const Button = React.forwardRef<
   HTMLAnchorElement | HTMLButtonElement,
   ButtonProps
->(({ className, buttonStyle = "default", children, ...props }, ref) => {
+>((props, ref) => {
   const baseClassName = cn(
     "px-[1em] py-[0.5em] transition-colors ease-[cubic-bezier(0.16,1,0.3,1)] duration-300 cursor-pointer",
     "flex items-center w-full max-w-[400px] gap-2",
-    buttonStyle === "secondary"
+    props.buttonStyle === "secondary"
       ? "bg-foreground/20 text-foreground border-foreground/20 hover:bg-foreground hover:text-background hover:border-foreground"
       : "bg-foreground text-background border-foreground hover:bg-foreground/10 hover:text-foreground hover:border-foreground/20",
-    props.disabled && "opacity-50 cursor-not-allowed",
-    className
+    props.className
   );
 
   // Use the 'href' prop to discriminate between the types
-  if ("href" in props && props.href) {
+  if (isLinkProps(props)) {
+    const { className, buttonStyle, ...rest } = props;
     return (
       <Link
         ref={ref as ForwardedRef<HTMLAnchorElement>}
         className={baseClassName}
-        {...props}
+        {...rest}
       >
-        {children}
+        {props.children}
       </Link>
     );
   }
 
+  const { className, buttonStyle, disabled, ...rest } = props;
+
   return (
     <button
       ref={ref as ForwardedRef<HTMLButtonElement>}
-      className={baseClassName}
-      {...props}
+      className={cn(
+        baseClassName,
+        props.disabled && "opacity-50 cursor-not-allowed"
+      )}
+      {...rest}
     >
-      {children}
+      {props.children}
     </button>
   );
 });
